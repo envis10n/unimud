7cd0772d9f8c19f74ac9d47bb226de8382a6d8a0
envis10n <envis10n@protonmail.com>
2019-04-01 16:26:45 -0500

    Update handlers index file.

--- src/modules/network/handlers/index.ts
+++ src/modules/network/handlers/index.ts
@@ -1,7 +1,11 @@
 import _connect from "./connect";
+import _message from "./message";
+import _json from "./json";
 
 namespace Handlers {
     export const connect = _connect;
+    export const message = _message;
+    export const json = _json;
 }
 
 export = Handlers;
